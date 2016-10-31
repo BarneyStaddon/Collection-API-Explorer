f3787c22c98687c07673a2a44a11f0bbbcfbcade
Barney Staddon <bstaddon@museumoflondon.org.uk>
2016-10-31 12:56:21 +0000

    Add chai testing

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var assert = require('chai').assert
+
+describe('Array', function() {
+	
+	it('should start empty', function() {
+    	
+    	var arr = [];
+    	assert.equal(arr.length, 0);
+  	
+  	});
+
+});
+
+
+
+
+
