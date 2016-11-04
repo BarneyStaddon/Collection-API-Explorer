45c7aac85be5ff650a76ca186cc17a60f9b539d3
Barney Staddon <bstaddon@museumoflondon.org.uk>
2016-11-04 11:57:41 +0000

    Remove comment

--- public/scripts/main.jsx
+++ public/scripts/main.jsx
@@ -184,6 +184,8 @@ class SearchContainer extends React.Component {
 
 //instantiates the root component, starts the framework, and injects the markup into a raw DOM element, provided as the second argument.
 ReactDOM.render(
+    
     <SearchContainer title="API Explorer" url="/api/search" />,
+    
     document.getElementById('content')
   );
